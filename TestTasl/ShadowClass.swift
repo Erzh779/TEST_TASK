80d9510e4a91adbb4ea5461a7d0f36875fb8cff1
Erzh779 <erzhan7taipov@mail.ru>
2022-01-19 18:36:36 +0600

    third commit

---
+++ TestTasl/ShadowClass.swift
@@ -0,0 +1,20 @@
+//
+//  ShadowClass.swift
+//  TestTasl
+//
+//  Created by Erzhan Taipov on 19.01.2022.
+//
+
+import UIKit
+
+class ShadowClass: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
